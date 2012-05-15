18961107e03c9393d786420b1d064f4261c7c931
Alexander Yarygin <yarygin.alexander@gmail.com>
2012-05-15 23:28:18 +0400

    modules/timer/timer.h: new interface definition

--- interfaces/modules/ITimer.h
+++ interfaces/modules/ITimer.h
@@ -12,44 +12,22 @@
 #include "IHardware.h"
 
 #include "types.h"
-#include "nodeEvent.h"
-#include "loggableEvent.h"
-#include "processableEvent.h"
 
-class ITimer : public IHardware 
+class ITimer : public IHardware, public QObject
 {
-    // Q_OBJECT
-
+    Q_OBJECT
 public:
+    ITimer()
+    {
+        interfaceInfo.name = "ITimer";
+        interfaceInfo.events["timerInterrupt"]["NodeID"] = "NodeID";
+        interfaceInfo.events["timerInterrupt"]["Type"] = "String";
+    }
+
     virtual VirtualTime getValue() = 0;
     virtual void setValue(VirtualTime newTime) = 0;
-    virtual void start(VirtualTime timeout, void (*timerInterrupt)(void*, QString), void* object, QString type) = 0;
-    virtual void stop(InterruptHandler timerInterrupt) = 0;
-    
-    class timerInterrupt : public nodeEvent
-// , public loggableEvent, public processableEvent
-    {
-    public:
-        timerInterrupt() { count++; }
-        void (*handler)(void*, QString);
-
-        void* object;
-
-        QString type;
-
-        static quint64 count;
-
-        virtual QString eventName() const { return "timerInterrupt"; }
-        // TODO: записать type
-        virtual void record(QDataStream& stream) { stream << eventNode; }
-        
-        virtual void process()
-        {
-            if (handler != NULL)
-                handler(object, type);
-        }
-        // TODO: add timer's ID
-    };
+    virtual void start(VirtualTime timeout, QString type) = 0;
+    virtual void stop(QString type) = 0;
 };
 Q_DECLARE_INTERFACE(ITimer,
                     "simulator.ITimer/0.1")
