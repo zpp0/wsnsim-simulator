22243736843a58a65f98b3934c295dda340b3d4a
Alexander Yarygin <yarygin.alexander@gmail.com>
2012-05-16 00:16:30 +0400

    modules/timer: module's implementation

--- modules/timer/timer.cpp
+++ modules/timer/timer.cpp
@@ -6,54 +6,18 @@
 
 #include "timer.h"
 
-#include "env.h"
-
-QString Timer::moduleName() const
+bool Timer::moduleInit(ISimulator* isimulator, QMap<QString, QString> params)
 {
-    return "Timer";
-}
+    m_parentNode = (INode*)isimulator->getCoreInterface(this, "INode");
+    m_env = (IEnv*)isimulator->getCoreInterface(this, "IEnv");
+    m_event = (IEvent*)isimulator->getCoreInterface(this, "IEvent");
 
-QString Timer::moduleVersion() const
-{
-    return "0.1";
-}
-
-QString Timer::moduleDescription() const
-{
-    return "Модуль таймера";
-}
-
-bool Timer::moduleInit(QList<ModuleParam> params)
-{
     return true;
-}
-
-QList<QString> Timer::moduleExportInterfaces() const
-{
-    QList<QString> tmp;
-    return tmp;
-}
-
-QList<QString> Timer::moduleImportInterfaces() const
-{
-    QList<QString> tmp;
-    return tmp;
-}
-
-QString Timer::deviceName() const
-{
-    return "Timer";
-}
-
-QList<InterruptHandler> Timer::interrupts()
-{
-    QList<InterruptHandler> tmp;
-    return tmp;
 }
 
 VirtualTime Timer::getValue()
 {
-    return Env::time;
+    return m_env->globalTime();
 }
 
 void Timer::setValue(VirtualTime newTime)
@@ -61,25 +25,13 @@ void Timer::setValue(VirtualTime newTime)
     // TODO: do
 }
 
-void Timer::start(VirtualTime timeout, void (*interrupt)(void*, QString), void* object, QString type)
+void Timer::start(VirtualTime timeout, QString type)
 {
-    qDebug() << "timer start at time" << getValue() + timeout;
-    timerInterrupt* Interrupt = new timerInterrupt();
-    qDebug() << "event count" << event::count;
-    // qDebug() << "time" << getValue() + timeout;
-    Interrupt->time = getValue() + timeout;
-    Interrupt->recordable = true;
-    // qDebug() << "nodeID " << m_parent->ID;
-    Interrupt->eventNode = m_parent->ID;
-    Interrupt->handler = interrupt;
-    Interrupt->object = object;
-    Interrupt->type = type;
-    Env::queue.insert(Interrupt);
+    m_event->post(this, "timerInterrupt", timeout,
+                  QVariantList() << m_parentNode->ID() << type);
 }
 
-void Timer::stop(InterruptHandler timerInterrupt)
+void Timer::stop(QString type)
 {
     // TODO: do
 }
-
-quint64 ITimer::timerInterrupt::count = 0;
