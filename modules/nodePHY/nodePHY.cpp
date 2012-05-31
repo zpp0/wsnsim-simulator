52eb1657985ac19af8bfa6fd1a11ef7139891ecb
Alexander Yarygin <yarygin.alexander@gmail.com>
2012-05-31 14:34:25 +0400

    from nodePHY removed message sending

--- modules/nodePHY/nodePHY.cpp
+++ modules/nodePHY/nodePHY.cpp
@@ -13,31 +13,15 @@ bool NodePHY::moduleInit(ISimulator* isimulator, QMap<QString, QString> params)
     // TODO: template
     m_parentNode = (INode*)isimulator->getCoreInterface(this, "INode");
     m_timer = (ITimer*)isimulator->getNodeInterface(this, m_parentNode, "ITimer");
-    m_csma = (ICSMA_CA*)isimulator->getNodeInterface(this, m_parentNode, "ICSMA_CA");
-    m_rtx = (Irtx*)isimulator->getNodeInterface(this, m_parentNode, "Irtx");
 
-    m_sendMessagePeriod = params["sendMessagePeriod"].toULong();
+    m_wakeUpPeriod = params["wakeUpPeriod"].toULong();
     
     return true;
 }
 
 void NodePHY::init()
 {
-    m_timer->start(m_sendMessagePeriod, "trySendMessage");
-}
-
-void NodePHY::trySendMessage()
-{
-    QByteArray message;
-        
-    char string[] = "hello";
-
-    message += "0000"; // преамбула
-    message += 0x7A; // SFD?
-    message += strlen(string); // length
-    message += string; // data
-
-    m_csma->sendMessage(message);
+    m_timer->start(m_wakeUpPeriod, "wakeUp");
 }
 
 void NodePHY::eventHandler(QString name, QVariantList params)
@@ -46,10 +30,8 @@ void NodePHY::eventHandler(QString name, QVariantList params)
     if (name == "nodePowerUp")
         init();
     if (name == "timerInterrupt")
-        if (params[1].toString() == "trySendMessage")
-            trySendMessage();
-    if (name == "CSMA_success" || name == "CSMA_fail")
-        trySendMessage();
+        if (params[1].toString() == "wakeUp")
+            init();
 }
 
 QT_BEGIN_NAMESPACE
