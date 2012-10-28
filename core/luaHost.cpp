2bfb0ac721e4eaf2cd990b893f5ace0a79ac4e01
Alexander Yarygin <yarygin.alexander@gmail.com>
2012-10-29 01:02:18 +0400

    check variables types on stack while getting event params in lua

--- core/luaHost.cpp
+++ core/luaHost.cpp
@@ -378,37 +378,44 @@ int LuaHost::postEvent(lua_State* lua)
 
                 switch (params[i].type) {
                 case INT32_TYPE:
-                    params[i].value.i32 = lua_tonumber(lua, -1);
+                    if (lua_isnumber(lua, -1))
+                        params[i].value.i32 = lua_tonumber(lua, -1);
                     break;
                 case BOOL_TYPE:
-                    params[i].value.b = lua_tonumber(lua, -1);
+                    if (lua_isnumber(lua, -1))
+                        params[i].value.b = lua_tonumber(lua, -1);
                     break;
                 case UINT8_TYPE:
-                    params[i].value.u8 = lua_tonumber(lua, -1);
+                    if (lua_isnumber(lua, -1))
+                        params[i].value.u8 = lua_tonumber(lua, -1);
                     break;
                 case UINT16_TYPE:
-                    params[i].value.u16 = lua_tonumber(lua, -1);
+                    if (lua_isnumber(lua, -1))
+                        params[i].value.u16 = lua_tonumber(lua, -1);
                     break;
                 case UINT32_TYPE:
-                    params[i].value.u32 = lua_tonumber(lua, -1);
+                    if (lua_isnumber(lua, -1))
+                        params[i].value.u32 = lua_tonumber(lua, -1);
                     break;
                 case UINT64_TYPE:
-                    params[i].value.u64 = lua_tonumber(lua, -1);
+                    if (lua_isnumber(lua, -1))
+                        params[i].value.u64 = lua_tonumber(lua, -1);
                     break;
                 case DOUBLE_TYPE:
-                    params[i].value.d = lua_tonumber(lua, -1);
+                    if (lua_isnumber(lua, -1))
+                        params[i].value.d = lua_tonumber(lua, -1);
                     break;
                 case STRING_TYPE:
-                {
-                    const char* string = lua_tostring(lua, -1);
-                    int length = strlen(string);
-                    char* strparam = new char(length);
-                    strcpy(strparam, string);
-                    params[i].value.string.data = strparam;
-                    params[i].value.string.length = length;
-                    lua_pop(lua, 1);
-                    break;
-                }
+                    if (lua_isstring(lua, -1)) {
+                        const char* string = lua_tostring(lua, -1);
+                        int length = strlen(string);
+                        char* strparam = new char(length);
+                        strcpy(strparam, string);
+                        params[i].value.string.data = strparam;
+                        params[i].value.string.length = length;
+                        lua_pop(lua, 1);
+                        break;
+                    }
                 case BYTE_ARRAY_TYPE:
                     // TODO: implement this
                     break;
